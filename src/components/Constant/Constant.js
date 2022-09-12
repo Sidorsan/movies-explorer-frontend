8b2b36eeb616764f05fa9c5d3f166179097cf6ca
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-09-12 13:12:11 +0700

    13:11

--- src/components/Constant/Constant.js
+++
@@ -1,6 +0,0 @@
-module.exports.staticWebSiteLink = "https://sidorsan.github.io/how-to-learn/";
-module.exports.adaptiveWebSiteLink =
-  "https://sidorsan.github.io/russian-travel/";
-module.exports.singlePageApplicationLink = "http://sidorsan.nomoredomains.sbs";
-module.exports.gitHabLink = "https://github.com/Sidorsan";
-module.exports.yandexPracticumLink = "https://practicum.yandex.ru/";
