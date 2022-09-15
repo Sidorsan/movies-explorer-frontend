5f34944f5b7b27b03030c975d98edf5e964b83cb
Aleksandr Sidorenko <sidor007@yandex.ru>
2022-09-15 15:41:24 +0700

    nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -18,9 +18,41 @@ function Header(props) {
       </a>
       <Switch>
         <Route path="/profile">
-          <Link className="header__link" to={"/navigation"}>
-            <img src={menu} alt="Меню" className="header__menu" />
-          </Link>
+          {/* <Link className="header__link" to={"/navigation"}> */}
+            <nav>
+              <div class="navbar">
+                <div class="container nav-container">
+                  <input class="checkbox" type="checkbox" name="" id="" />
+                  <div class="hamburger-lines">
+                    <span class="line line1"></span>
+                    <span class="line line2"></span>
+                    <span class="line line3"></span>
+                  </div>
+                  <div class="logo">
+                    <h1>Navbar</h1>
+                  </div>
+                  <div class="menu-items">
+                    <li>
+                      <a href="#">Home</a>
+                    </li>
+                    <li>
+                      <a href="#">about</a>
+                    </li>
+                    <li>
+                      <a href="#">blogs</a>
+                    </li>
+                    <li>
+                      <a href="#">portfolio</a>
+                    </li>
+                    <li>
+                      <a href="#">contact</a>
+                    </li>
+                  </div>
+                </div>
+              </div>
+            </nav>
+            {/* <img src={menu} alt="Меню" className="header__menu" /> */}
+          {/* </Link> */}
         </Route>
 
         <Route path="/">
