5d1977d3ddd20488e487d2350ed7462823f81801
Sidor007 <Sidor007@yandex.ru>
2022-10-03 21:17:34 +0700

    SearchFormNerabotaet1

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,4 +1,3 @@
-
 //react-hook-form.com/advanced-usage
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
@@ -6,27 +5,45 @@ import { useForm } from "react-hook-form";
 import find from "../../../images/find.svg";
 import findInInput from "../../../images/findInInput.svg";
 import Form from "../../Form/Form";
+import { clear } from '@testing-library/user-event/dist/clear';
 
 const SearchForm = (props) => {
-  const [data, setData] = useState({
-    film: ""
-  });
-console.log(data);
-  const handleChange = (e) => {
-    setData(e.target.value);
-    // const { name, value } = e.target;
-    // setData({
-    //   ...data,
-    //   [name]: value,
-    // });
-  };
+  const [film, setFilm] = React.useState("");
+
+  console.log(film);
+  function handleChangeFilm(e) {
+    setFilm(e.target.value);
+  }
+  // const [data, setData] = useState({
+  //   film: ""
+  // });
+
+  // const handleChange = (e) => {
+  //   setData(e.target.value);
+  //   // const { name, value } = e.target;
+  //   // setData({
+  //   //   ...data,
+  //   //   [name]: value,
+  //   // });
+  // };
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { errors },
   } = useForm();
 
+  //  function handleSubmit(e) {
+  //    e.preventDefault();
+  //    console.log("ecac");
+  //  }
+
+  const onSubmit = (e) => {
+       console.log(e);
+       reset()
+
+  };
   // const onSubmit = (data) => {
   //   const { film } = data;
   //   console.log(data);
@@ -34,13 +51,13 @@ console.log(data);
 
   return (
     <div className="searchForm">
-      <form onSubmit={handleSubmit(props.onSubmit)} className="searchForm__form">
+      <form onSubmit={handleSubmit(onSubmit)} className="searchForm__form">
         <input
           className="searchForm__input"
           placeholder="Фильм"
           id="film"
           name="film"
-          // onChange={handleChange}
+          onChange={handleChangeFilm}
           {...register("film", {
             required: true,
           })}
@@ -51,15 +68,10 @@ console.log(data);
           )}
         </span>
 
-        <button
-          type="submit"
-          // onSubmit={handleSubmit}
-          className="searchForm__submitButton"
-        >
+        <button type="submit" className="searchForm__submitButton">
           <img src={find} alt="Лупа" />
         </button>
       </form>
-
     </div>
   );
 };
